3a7a8baa296ad2e0fa6c64825ccc8d27e29e20f2
Mateus Inocentini <mateus.inocentini@e2etec.com.br>
2022-02-23 10:44:58 -0300

    Fist commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace QueryDePara_BKO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txDatabase = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txSchema = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txTable = new System.Windows.Forms.TextBox();
+            this.cbTruncate = new System.Windows.Forms.CheckBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btOpenFile = new System.Windows.Forms.Button();
+            this.txPlanilha = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txNomenclaturaComercial = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txNomenclaturaIntergrall = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txNomenclaturaAtlysPlanos = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txNomenclaturaAtlysBonusFranquia = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txPrecoPlano = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbValoresLidos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // txDatabase
+            // 
+            resources.ApplyResources(this.txDatabase, "txDatabase");
+            this.txDatabase.Name = "txDatabase";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // txSchema
+            // 
+            resources.ApplyResources(this.txSchema, "txSchema");
+            this.txSchema.Name = "txSchema";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // txTable
+            // 
+            resources.ApplyResources(this.txTable, "txTable");
+            this.txTable.Name = "txTable";
+            // 
+            // cbTruncate
+            // 
+            resources.ApplyResources(this.cbTruncate, "cbTruncate");
+            this.cbTruncate.Name = "cbTruncate";
+            this.cbTruncate.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+            // 
+            // btOpenFile
+            // 
+            resources.ApplyResources(this.btOpenFile, "btOpenFile");
+            this.btOpenFile.Name = "btOpenFile";
+            this.btOpenFile.UseVisualStyleBackColor = true;
+            this.btOpenFile.Click += new System.EventHandler(this.btOpenFile_Click);
+            // 
+            // txPlanilha
+            // 
+            resources.ApplyResources(this.txPlanilha, "txPlanilha");
+            this.txPlanilha.Name = "txPlanilha";
+            this.txPlanilha.ReadOnly = true;
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // txNomenclaturaComercial
+            // 
+            resources.ApplyResources(this.txNomenclaturaComercial, "txNomenclaturaComercial");
+            this.txNomenclaturaComercial.Name = "txNomenclaturaComercial";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // txNomenclaturaIntergrall
+            // 
+            resources.ApplyResources(this.txNomenclaturaIntergrall, "txNomenclaturaIntergrall");
+            this.txNomenclaturaIntergrall.Name = "txNomenclaturaIntergrall";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // txNomenclaturaAtlysPlanos
+            // 
+            resources.ApplyResources(this.txNomenclaturaAtlysPlanos, "txNomenclaturaAtlysPlanos");
+            this.txNomenclaturaAtlysPlanos.Name = "txNomenclaturaAtlysPlanos";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // txNomenclaturaAtlysBonusFranquia
+            // 
+            resources.ApplyResources(this.txNomenclaturaAtlysBonusFranquia, "txNomenclaturaAtlysBonusFranquia");
+            this.txNomenclaturaAtlysBonusFranquia.Name = "txNomenclaturaAtlysBonusFranquia";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // txPrecoPlano
+            // 
+            resources.ApplyResources(this.txPrecoPlano, "txPrecoPlano");
+            this.txPrecoPlano.Name = "txPrecoPlano";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // lbValoresLidos
+            // 
+            resources.ApplyResources(this.lbValoresLidos, "lbValoresLidos");
+            this.lbValoresLidos.Name = "lbValoresLidos";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbValoresLidos);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txPrecoPlano);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txNomenclaturaAtlysBonusFranquia);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txNomenclaturaAtlysPlanos);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txNomenclaturaIntergrall);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txNomenclaturaComercial);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txPlanilha);
+            this.Controls.Add(this.btOpenFile);
+            this.Controls.Add(this.cbTruncate);
+            this.Controls.Add(this.txTable);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txSchema);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txDatabase);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txDatabase;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txSchema;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txTable;
+        private System.Windows.Forms.CheckBox cbTruncate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btOpenFile;
+        private System.Windows.Forms.TextBox txPlanilha;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txNomenclaturaComercial;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txNomenclaturaIntergrall;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txNomenclaturaAtlysPlanos;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txNomenclaturaAtlysBonusFranquia;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txPrecoPlano;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbValoresLidos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueryDePara_BKO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Selecionar Planilha Excel";
+            openFileDialog1.Filter = "Excel|*.xlsx";
+            openFileDialog1.ShowDialog();
+
+            txPlanilha.Text = openFileDialog1.FileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "";
+            if (cbTruncate.Checked)
+                query += $"TRUNCATE TABLE [{txDatabase.Text}].[{txSchema.Text}].[{txTable.Text}];\n";
+
+            query += $"INSERT INTO [{txDatabase.Text}].[{txSchema.Text}].[{txTable.Text}] (nomenclatura_comercial, nomenclatura_intergrall, nomenclatura_atlys_planos, nomenclatura_atlys_bonus_franquia, preco_plano) VALUES \n";
+
+            int nomenclatura_comercial = Convert.ToInt32(txNomenclaturaComercial.Text);
+            int nomenclatura_intergrall = Convert.ToInt32(txNomenclaturaIntergrall.Text);
+            int nomenclatura_atlys_planos = Convert.ToInt32(txNomenclaturaAtlysPlanos.Text);
+            int nomenclatura_atlys_bonus_franquia = Convert.ToInt32(txNomenclaturaAtlysBonusFranquia.Text);
+            int preco_plano = Convert.ToInt32(txPrecoPlano.Text);
+            int count = 0;
+            FileInfo f = new FileInfo(txPlanilha.Text);
+            var package = new ExcelPackage(f);
+            ExcelWorksheet ws = package.Workbook.Worksheets[0];
+            for (int i = 2; i <= ws.Dimension.End.Row; i++)
+            {
+                if (ws.Cells[i, nomenclatura_comercial].Value == null)
+                    continue;
+#pragma warning disable S1643 // Strings should not be concatenated using '+' in a loop
+                string auxPrecoPlano = ws.Cells[i, preco_plano].Value.ToString().Trim();
+                auxPrecoPlano = Regex.Replace(auxPrecoPlano, "R\\$", "");
+                double precoPlano = Convert.ToDouble(auxPrecoPlano);
+                string auxAtlysBonusFranquia = ws.Cells[i, nomenclatura_atlys_bonus_franquia].Value == null ? "" : ws.Cells[i, nomenclatura_atlys_bonus_franquia].Value.ToString();
+
+                query += $"('{ws.Cells[i, nomenclatura_comercial].Value.ToString()}', '{ws.Cells[i, nomenclatura_intergrall].Value.ToString()}', '{ws.Cells[i, nomenclatura_atlys_planos].Value.ToString()}', '{auxAtlysBonusFranquia}', '{precoPlano.ToString("0.##")}'),\n";
+#pragma warning restore S1643 // Strings should not be concatenated using '+' in a loop
+                count++;
+                /*
+                item.Tarefa = ws.Cells[i, 1].Value.ToString();
+                item.Tipo = ws.Cells[i, 2].Value.ToString();
+                item.LocalNumLP = ws.Cells[i, 3].Value.ToString();
+                item.MesConta = ws.Cells[i, 4].Value.ToString();
+                item.ValorNFOriginal = ws.Cells[i, 5].Value.ToString();
+                item.ValorNFCorrigida = ws.Cells[i, 6].Value.ToString();
+                item.VencimentoProrrogacao = ws.Cells[i, 7].Value.ToString();
+                item.CCM = ws.Cells[i, 8].Value.ToString();
+                item.Protocolo = ws.Cells[i, 9].Value.ToString();
+                item.LoginCSO = ws.Cells[i, 10].Value.ToString();
+                item.DataTransacao = ws.Cells[i, 11].Value.ToString();
+                */
+            }
+            lbValoresLidos.Text = $"Valores Lidos: {count}";
+            query = query.Remove(query.Length - 2, 2);
+            richTextBox1.Text = query;
+        }
+    }
+}
